91667c22d19d4badc4afee368bb94405f3d214f1
samanthavandapuye <samantha.vandapuye@gmail.com>
2013-03-13 13:44:32 -0400

    new media

--- wp-content/themes/thebushels/download.php
+++ wp-content/themes/thebushels/download.php
@@ -7,9 +7,9 @@ get_header(); ?>
 
 <div id="wrapper">
   <div align="left">
-  <p><a href="<?php echo get_template_directory_uri(); ?>/media/06_cortez_1.mp3.zip">DOWNLOAD YOUR SONG</a></p>
+  <p><a href="<?php echo get_template_directory_uri(); ?>/media/04_Cherokee_Girl.mp3.zip">DOWNLOAD YOUR SONG</a></p>
   <p>Thanks for your interest and enjoy the song.</p><p>&nbsp;</p><p>&nbsp;</p><p>
-<a href="/home"><strong>Take me back to the home page &#187 </strong></a></p></div>
+<a href="/home"><strong>Take me back to the home page &#187; </strong></a></p></div>
 </div>
 
 <?php get_footer(); ?>
